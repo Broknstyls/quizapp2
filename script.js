64a94b49a96cd7cad39d56dba3e796febdc1faef
Broknstyls <muban90@gmail.com>
2023-11-07 14:01:22 +0200

    still not stable

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-
+let strike = function(){
+   let number = Number(document.getElementById('strikes').textContent)
+   number--;
+    
+}
 document.getElementById('start').onclick = function(){
     let question = Math.floor(Math.random()*30)
+    
     document.getElementById('question').textContent = question;
+   let question2 = question*question;
+   console.log(question2)
 }
 
 document.getElementById('submit').onclick = function(){
     let answer = document.getElementById('answer').textContent;
-    answer = Number(answer);
-    ;
-    let correct = Number(document.getElementById('question').textContent);
-correct = correct*correct
+    
+    
+    let correct = document.getElementById('question').textContent;
+    correct = correct*correct;
+
     if(answer == correct){
         document.getElementById('result').textContent = 'CORRECT'
+        
     } else{
         document.getElementById('result').textContent = 'WRONG'
+        strike();
+        
     }
 }
