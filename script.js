cd1bd91f8431388766e34f5ac1760e82e8488259
Broknstyls <muban90@gmail.com>
2023-11-07 12:50:21 +0200

    added button functions,buggy

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.getElementById('')
+let question = Math.floor(Math.random()*30)
+
+document.getElementById('start').onclick = function(){
+    let question = Math.floor(Math.random()*30)
+    document.getElementById('question').textContent = question;
+}
+
+document.getElementById('submit').onclick = function(){
+    let answer = document.getElementById('answer').textContent;
+    answer = Number(answer);
+    ;
+    let correct = Number(document.getElementById('question').textContent);
+correct = correct*correct
+    if(answer == correct){}
+}
