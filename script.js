7f9449ef8d431289f2e01b0ac3eb4c3b9e8bdfe8
Broknstyls <muban90@gmail.com>
2023-11-08 14:24:38 +0200

    start button works properly

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
 let strike = document.getElementById('strikes').textContent;
 
-    
-
 document.getElementById('start').onclick = function(){
     let question = Math.floor(Math.random()*30)
-    
-    document.getElementById('question').textContent = question;
-   let question2 = question*question;
-   console.log(question2)
+    document.getElementById('question').textContent = question
 }
 
-document.getElementById('submit').onclick = function(){
-    let answer = document.getElementById('answer').textContent;
-    let correct = document.getElementById('question').textContent;
-    correct = correct*correct;
-
-    if(answer == correct){
-        document.getElementById('result').textContent = 'CORRECT'
-
-    } else{
-        document.getElementById('result').textContent = 'WRONG'
-        strike--;
-        
-    }
-}
