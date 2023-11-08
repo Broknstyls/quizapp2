e79cee74c957c6faa26bbd456a5c1f131319a391
Broknstyls <muban90@gmail.com>
2023-11-08 14:41:45 +0200

    submit works, adding extra features

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let strike = document.getElementById('strikes').textContent;
-
+let strike = 3
+let score = document.getElementById('score').value;
 document.getElementById('start').onclick = function(){
     let question = Math.floor(Math.random()*30)
     document.getElementById('question').textContent = question
@@ -8,6 +8,15 @@ document.getElementById('start').onclick = function(){
 document.getElementById('submit').onclick = function(){
     let base = document.getElementById('question').textContent;
     power = base * base;
-    console.log(power);
-    
+    let answer = document.getElementById('answer').value;
+    if(answer == power){
+        document.getElementById('result').textContent = 'CORRECT!';
+        score++;
+    }    else{
+        document.body.style.background = 'red';
+        document.getElementById('result').textContent = 'WRONG!!!!';
+        strike--;
+        document.getElementById('strikes').value = strike;
+
+    }
 }
