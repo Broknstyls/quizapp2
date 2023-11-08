0145560d859523a10815c02fec07204ae6307417
Broknstyls <muban90@gmail.com>
2023-11-08 14:49:44 +0200

    added scoring system

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let strike = 3
-let score = document.getElementById('score').value;
+
 document.getElementById('start').onclick = function(){
     let question = Math.floor(Math.random()*30)
     document.getElementById('question').textContent = question
+    document.getElementById('whole').style.background=' rgb(60, 70, 78)';
 }
 
 document.getElementById('submit').onclick = function(){
+    let strike = 3;
+    let score = 0;
     let base = document.getElementById('question').textContent;
     power = base * base;
     let answer = document.getElementById('answer').value;
@@ -13,11 +15,12 @@ document.getElementById('submit').onclick = function(){
         document.getElementById('whole').style.background = 'green';
         document.getElementById('result').textContent = 'CORRECT!';
         score++;
+        document.getElementById('score').innerHTML= score;
     }    else{
         document.getElementById('whole').style.background = 'red';
         document.getElementById('result').textContent = 'WRONG!!!!';
         strike--;
-        document.getElementById('strikes').value = strike;
+        document.getElementById('strikes').innerHTML= strike;
 
     }
 }
